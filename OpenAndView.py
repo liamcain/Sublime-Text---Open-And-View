fec2c940f39fba16fe80ec7f137d2a9286ef0859
Derek Watts <wattsin@gmail.com>
2011-12-30 10:05:08 -0500

    Update OpenAndView.py

--- OpenAndView.py
+++ OpenAndView.py
@@ -1,16 +1,7 @@
-#	Simply add something like this to the top of each of your files.
-#	//LOCATION: /controllerName/
-#	When you hit Shift+Command+m this plugin will check the top 10 lines of the current file
-#	look for the location tag and append it to the URL defined below
-#	So in this example it will open http://localhost:8888/controllerName/
-#	If the programming location used needs a different identifier for comments, simply change // to whatever.
-
 #	Key Binding
 #	[
 #		{ "keys": ["super+shift+m"], "command": "open_and_view" }
-#	]	
-
-#	I use this code together with Code Igniter framework
+#	]
 
 import sublime, sublime_plugin
 import webbrowser
